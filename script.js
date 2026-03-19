8aa627493a7302103716e388292d4b5fbabaeb52
oatkrunch <suyalsahukhala787@gmail.com>
2026-03-19 20:40:51 +0545

    AQI monitor and WeatherForcast App

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ const info = document.querySelector(".weatherInfo")
 const emoji = document.querySelector(".weatheremoji")
 const card = document.querySelector(".displayWeatherCard")
 const aqiCard= document.querySelector(".displayAQICard")
-// const indicators = document.querySelector(".indicatorValues")
 
 const apiKey_weather = "ba7145b1f075417c5b7241a664bf0382";
 const apiKey_AQI = "45dfac023f4d364d8b22b246a25a83c76149208d";
@@ -49,11 +48,12 @@ form.addEventListener("submit", async function(e){
             card.textContent=""
             card.style.display= "flex"
             card.style.flexDirection = "column"
+            card.style.alignItems = "center"
             card.style.gap = "10px"
             card.style.padding = "10px"
             card.style.width = "320px"
             card.style.height= "510px"
-            card.style.backgroundColor = "#ffffff"
+            card.style.backgroundColor = `${changeBackgroundCard(id)}`
             card.style.border = "1px white"
             card.style.borderRadius = "12px"
             card.style.boxShadow = "0px 4px 15px rgba(0,0,0,0.1)"
@@ -64,7 +64,7 @@ form.addEventListener("submit", async function(e){
             const tempDisplay = document.createElement("p")
             const humidityDisplay = document.createElement("p")
             const descDisplay = document.createElement("p")
-            const emojiDisplay = document.createElement("p")
+            const emojiDisplay = document.createElement("img")
                 
             cityDisplay.innerHTML = city
             cityDisplay.className = "city"
@@ -84,8 +84,10 @@ form.addEventListener("submit", async function(e){
             descDisplay.className = "weather"
             card.appendChild(descDisplay)
 
-            emojiDisplay.innerHTML = `${displayWeatherEmoji(id)}`
-            emojiDisplay.className = "weatheremoji"
+            emojiDisplay.src = `${displayWeatherEmoji(id)}`
+            emojiDisplay.alt = "image is only in the hosts computer so it wont display here"
+            emojiDisplay.width = 110
+            emojiDisplay.height = 110
             card.appendChild(emojiDisplay)
             
 
@@ -95,21 +97,45 @@ form.addEventListener("submit", async function(e){
        
         };
         function displayWeatherEmoji(id){
+          console.log("displayed")
             switch(true){
               case (id >= 200 && id < 300):
-                return `🌩️`
+                return `WeatherIcons/thunderstorm200-300.png`
               case (id>=300 && id<400):
-                return `🌦️`
+                return `WeatherIcons/drizzle300-400.png`
               case (id>=500 && id<600):
-                return `🌧️`
+                return `WeatherIcons/rain500-600.png`
               case (id>=600 && id<700):
-                return `❄️`
-              case (id>800 && id<810):
-                return `☁️`
+                return `WeatherIcons/snowflake600-700.png`
+              case (id>=700 && id<800):
+                return `WeatherIcons/wind700-800.png`
               case (id == 800):
-                return `☀️`
+                return `WeatherIcons/sun-800.png`
+              case (id>800 && id<900):
+                return `WeatherIcons/cloud800-900.png`
               default:
-                return `🌤️`
+                return ``
+            }
+        }
+
+        function changeBackgroundCard(id){
+           switch(true){
+              case (id >= 200 && id < 300):
+                return `#c13d52`
+              case (id>=300 && id<400):
+                return `#ac526c`
+              case (id>=500 && id<600):
+                return `#23a3b0`
+              case (id>=600 && id<700):
+                return `#55787c`
+              case (id>=700 && id<800):
+                return `#88a660`
+              case (id == 800):
+                return `#ec6640`
+              case (id>800 && id<900):
+                return `#ecc951`
+              default:
+                return `#6a4a3d`
             }
         }
         function displayError(message){
@@ -162,10 +188,10 @@ form.addEventListener("submit", async (e) =>{
               aqiCard.style.flexDirection = "column"
               aqiCard.style.alignItems = "center"
               aqiCard.style.padding = "20px"
-              aqiCard.style.gap = "12px"
+              aqiCard.style.gap = "18px"
               aqiCard.style.backgroundColor = "#ffffff"
-              aqiCard.style.width = "220px"
-              aqiCard.style.height = "300px"
+              aqiCard.style.width = "230px"
+              aqiCard.style.height = "350px"
               aqiCard.style.borderRadius = "10px"
               aqiCard.style.boxShadow = "0px 4px 15px rgba(0,0,0,0.1)"
 
@@ -189,10 +215,14 @@ form.addEventListener("submit", async (e) =>{
                displayCity.style.margin = "0px"
 
                displayAQI.textContent = `${aqi}`
+               displayAQI.style.color = `${displayAQITextColor(aqi)}`
+               displayAQI.style.fontWeight = "600"
                displayAQI.className = "aqiValue"
                displayAQI.style.margin = "0px"
 
                displayStatus.textContent = `${showStatus(aqi)}`
+               displayStatus.style.color = `${displayAQITextColor(aqi)}`
+               displayAQI.style.fontWeight = "bold"
                displayStatus.className = "aqiInfo"
                displayStatus.style.margin = "0px"
 
@@ -209,7 +239,7 @@ form.addEventListener("submit", async (e) =>{
               aqiCard.appendChild(displayPM)
                
           }else{
-               displayError("Response is not okay")
+               displayError("Cannot fetch AQI of this location")
           }
                    
      }
@@ -241,15 +271,15 @@ form.addEventListener("submit", async (e) =>{
 
      function  showStatus(aqi){
           switch(true){
-               case(0 <= aqi <=50):
+               case(0 <= aqi && aqi<=50):
                     return `• Good`
-               case(51 <= aqi <=100):
+               case(51 <= aqi && aqi <=100):
                     return `• Moderate`
-               case(101<= aqi <=150):
+               case(101<= aqi && aqi<=150):
                     return `• Unhealthy for sensitive groups`
-               case(151 <= aqi <=200):
+               case(151 <= aqi && aqi<=200):
                     return `• Unhealthy`
-               case(201 <= aqi <=300):
+               case(201 <= aqi && aqi<=300):
                     return `• Very Unhealthy`
                case(301 <= aqi):
                     return `• Hazardous`
@@ -257,7 +287,22 @@ form.addEventListener("submit", async (e) =>{
                     return `• unrecognized`
           }
      }
-     function displayAQITextColor(){
-
+     function displayAQITextColor(aqi){
+           switch(true){
+               case(0 <= aqi && aqi<=50):
+                    return `#64d49c`
+               case(51 <= aqi && aqi <=100):
+                    return `#edce35`
+               case(101<= aqi && aqi<=150):
+                    return `#efb42c`
+               case(151 <= aqi && aqi<=200):
+                    return `#ef8924`
+               case(201 <= aqi && aqi<=300):
+                    return `#ef5e1b`
+               case(301 <= aqi):
+                    return `#ef3711`
+               default:
+                    return `white`
+          }
      }
 })
